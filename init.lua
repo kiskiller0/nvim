11bd06e5cf7157eefa43c72d3c47d0ca1847fd44
kiskiller0 <kiskiller0@gmail.com>
2024-06-18 22:38:22 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("bader.keymaps")
+require("bader.options")
+require("bader.plugins")
+
+
+-- setting up plugins --
+require("bader.plugins.comment")
+require("bader.plugins.autopairs")
---
+++ lua/bader/keymaps.lua
@@ -0,0 +1,78 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.keymap.set
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv^", opts)
+keymap("v", ">", ">gv^", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+
+-- Copy paste --
+keymap("n", "<C-c>", "yy", opts)
+keymap("v", "<C-c>", "y", opts)
+keymap("n", "<C-v>", "p", opts)
+keymap("v", "<C-v>", "p", opts)
+
+-- save --
+keymap("n", "<C-s>", ":w<Cr>", opts)
+keymap("i", "<C-s>", "<Esc>:w<Cr>", opts)
+keymap("v", "<C-s>", ":w<Cr>", opts)
+
+-- double line --
+keymap("n", "<C-d>", "yyp", opts)
+keymap("i", "<C-d>", "<Esc>yypI", opts)
+
+
+-- zen mode --
+keymap("n", "<Space>gh", ":colo industry<Cr>:syntax off<Cr>:set norelativenumber<Cr>:set nonumber<Cr>", opts)
+keymap("n", "<Space>ge", ":colo default<Cr>:syntax on<Cr>:set relativenumber<Cr>:set number<Cr>", opts)
